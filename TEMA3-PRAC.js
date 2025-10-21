1d96c5d6afc57cba3b5946c2cd23dffabf53e43a
joan <joan.mmillan06@gmail.com>
2025-10-21 22:35:03 +0200

    Añadiendo metodos , ahora se actualizan los datos de num.hijos , poblacion , Codigo postal y la edad

--- TEMA3-PRAC.js
+++ TEMA3-PRAC.js
@@ -15,10 +15,45 @@ function actualizar(){
     console.log("idiomas: " + idiomas);
     console.log("hijos: " + hijos);
 
+    //Hacer Mayus el nombre y juntar el apellido y nombre en una cadena de texto
     var nombreMayus = nombre.toUpperCase();
-    
     var nombreYapellidos = apellidos +", " +nombreMayus;
+    
+    //Calcular la edad de el usuario
+    var edad = new Date() - fecha;
+    var edadFinal = Math.floor(edad/(1000 * 60 * 60 * 24 * 365.25)) 
+
+    //Separar el codigo postal y la poblacion a la que pertenece el usuario
+    var codigopostal = poblacion.split("-");
+    var ciudad = codigopostal[1];
+    var postal = codigopostal[0];
+    
+    //Contar la cantidad de hijos que se han introducido 
+    var hijosNombres = hijos.split("-");
+    var CantidadHijos = hijosNombres.length;
 
     document.getElementById('nombreyapellidos').textContent = nombreYapellidos;
+    document.getElementById('edad').textContent = edadFinal;
+    document.getElementById('codigoPostal').textContent = postal;
+    document.getElementById('poblado').textContent = ciudad;
+    document.getElementById('numeroHijos').textContent = CantidadHijos;
+
+    
+
+    /*
+    for(var i = 0 ; i < CantidadHijos ; i++){
+        const columna = document.getElementById('hijosInsertar');
+        
+        if(columna){
+
+            var newHijo = document.createElement('tr');
+            newHijo.textContent = hijosNombres[i]
+            columna.appendChild(newHijo);
+            console.log("celda creada");
+        }else{
+            console.log("No va");
+        }
+
+    }*/
 }
 
